4edbb49168626faa1f01ab187d1d9151d8b9979b
flucloxin500 <mdwalid826@gmail.com>
2023-09-18 22:01:50 +0600

    Live Test with listile

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
       title: 'Ostad App',
     );
@@ -16,126 +16,50 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.greenAccent
-        ),
-        child: Scrollbar(
-          thickness: 20,
-          radius: Radius.circular(10),
-          child: GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3),
-            children: [
-              Icon(Icons.add_business),
-              Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black38,
-                    border: Border.all(color: Colors.amber,width: 2),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Icon(Icons.add)),
-              Icon(Icons.ac_unit),
-              Icon(Icons.account_circle),
-              Icon(Icons.add_business_outlined),
-              Icon(Icons.account_balance_wallet_outlined),
-              Icon(Icons.abc),
-              Icon(Icons.adb_rounded),
-              Icon(Icons.accessibility_new_sharp),
-              Icon(Icons.add_business),
-              Icon(Icons.account_balance_wallet),
-              Icon(Icons.accessible_rounded),
-              Icon(Icons.add_card_outlined),
-              Icon(Icons.accessible_rounded),
-              Icon(Icons.add_chart_outlined),
-            ],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.shopping_cart),
           ),
-        ),
+        ],
+
+        title: Text('My Shopping List'),
+        centerTitle: true,
+      ),
+      body:Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('OneUI',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('iOS',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+            ),),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('Octopus OS'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('MIUI'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('Oxygen OS'),
+          ),
+
+        ],
       )
-          /*
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.lightGreen
-        ),
-        child: Scrollbar(
 
-        child: SingleChildScrollView(
 
-          // direction of scroll area
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
-
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.black38,width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(48)),
-
-                ),
-                child: ListTile(
-                  title: Text('Username'),
-                  subtitle: Text('user@gmnail.com'),
-                  leading: Icon(Icons.adb_outlined),
-                  trailing: Icon(Icons.arrow_right_alt),
-                  onTap: (){
-                    print('Last Title pressed');
-                  },
-                ),
-              ),
-              Divider(
-                height: 5,
-                thickness: 1,
-                color: Colors.black38,
-                endIndent: 25,
-                indent: 16,
-              ),
-              ListTile(
-                title: Text('Username'),
-                subtitle: Text('user@gmnail.com'),
-                leading: Icon(Icons.adb_outlined),
-                trailing: Icon(Icons.arrow_right_alt),
-                onTap: (){
-                  print('Last Title pressed');
-                },
-              ),
-              ListTile(
-                title: Text('Username'),
-                subtitle: Text('user@gmnail.com'),
-                leading: Icon(Icons.adb_outlined),
-                trailing: Icon(Icons.arrow_right_alt),
-
-                onTap: (){
-                  print('Last Title pressed');
-                },
-              ),
-              Divider(
-                height: 5,
-                thickness: 1,
-                color: Colors.pink,
-                endIndent: 25,
-                indent: 16,
-              ),
-              ListTile(
-                title: Text('Username'),
-                subtitle: Text('user@gmnail.com'),
-                leading: Icon(Icons.adb_outlined),
-                trailing: Icon(Icons.arrow_right_alt),
-                onTap: (){
-                  print('Last Title pressed');
-                },
-              ),
-          ],
-        ),
-    ),
-        ),
-
-     ),*/
-    );
-
+          );
   }
 
 }
